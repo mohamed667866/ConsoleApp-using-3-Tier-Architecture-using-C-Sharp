754c044d39932b2852bee6eb326223f04f9917b5
mohamed66786 <mohamedtarek70900@gmail.com>
2024-08-21 21:47:20 +0300

    First Push

---
+++ ContactsDataAccessLayer/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ContactsDataAccessLayer
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=localhost\\MSSQLSERVER1;Database=ContactsDB;User Id=sa;Password=123456;";
+       
+
+    }
+}
